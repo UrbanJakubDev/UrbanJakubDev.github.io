e43f5ca25d875091c7eeb9de990eda789b9f3161
jakub.urban <jakub.73@hotmail.com>
2021-02-11 17:22:52 +0100

    added axios and login layout

---
+++ layouts/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="default_layout page_container">
+    <div class="base_page">
+      <Nuxt />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import style from '../assets/styles/style.scss'
+export default {}
+</script>
+
+<style>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'static/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
