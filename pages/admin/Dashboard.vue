bf32606c899e348b0309809927b9b9d6a3a6ada9
jakub.urban <jakub.73@hotmail.com>
2021-02-12 21:58:07 +0100

    add admin dashboard

---
+++ pages/admin/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="content__container">
+    Dashboard
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'admin'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
