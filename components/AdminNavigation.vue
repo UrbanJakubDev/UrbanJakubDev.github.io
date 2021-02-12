8cc712bbae0e7d186523e458d246787bc9ddc73f
jakub.urban <jakub.73@hotmail.com>
2021-02-12 21:58:27 +0100

    add admin menu and exit main menu

---
+++ components/AdminNavigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="admin__navigation__container">
+    <h3>Admin Menu</h3>
+    <div class="admin__navbar">
+      <div class="nav__links">
+        <NuxtLink to="/about">About Me</NuxtLink>
+        <NuxtLink to="/portfolio">Portfolio</NuxtLink>
+        <NuxtLink to="/cv">CV</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+      menuIcon: 'menu',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.admin__navigation__container{
+  padding-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  background-color: crimson;
+}
+
+.admin__navbar{
+
+}
+
+.nav__links{
+display: flex;
+  flex-direction: column;
+
+}
+
+</style>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,36 +1,86 @@
 <template>
   <div class="navigation__container">
-    <Logo />
-    <div class="navbar" v-bind:class="{ show: isActive }">
-      <div class="nav__links">
-        <NuxtLink to="/about">About Me</NuxtLink>
-        <NuxtLink to="/portfolio">Portfolio</NuxtLink>
-        <NuxtLink to="/cv">CV</NuxtLink>
-      </div>
-      <ThemeMod />
+    <div class="nav__bar">
+      <Logo />
+      <ThemeMod class="nav-bar-theme-icon" />
     </div>
-    <span class="material-icons medium dropdown-icon" @click="showPopupMenu()">
-      {{ menuIcon }}
-    </span>
+    <div class="nav__links">
+      <NuxtLink to="/about">O mě</NuxtLink>
+      <NuxtLink to="/portfolio">Portfolio</NuxtLink>
+      <NuxtLink to="/cv">CV</NuxtLink>
+      <ThemeMod class="nav-links-theme-icon" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      isActive: false,
-      menuIcon: 'menu',
-    }
-  },
-  methods: {
-    showPopupMenu() {
-      this.isActive = !this.isActive
-      this.menuIcon = !this.isActive ? 'menu' : 'menu_open'
-    },
-  },
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
+/* Small Screen 320px and more */
+
+.nav__bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav__links {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  text-align: center;
+}
+
+.nav-links-theme-icon {
+  display: none;
+}
+
+a {
+  transition: all .15s ease-in-out;
+  text-align: center;
+  border-bottom: 2px solid var(--color-text-secondary);
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  width: 33%;
+  font-size: 1.1em;
+  font-weight: bold;
+  text-decoration: none;
+
+  &:hover {
+    box-shadow: inset 0 -3.25em 0 0 var(--color-text-secondary);
+  }
+}
+
+/* Larger than tablet */
+@media (min-width: 750px) {
+  .navigation__container {
+    display: flex;
+    justify-content: space-between;
+  }
+  .nav__links {
+    width: 300px;
+    display: flex;
+  }
+
+  a {
+    margin-right: 2rem;
+    width: 100%;
+    border: none;
+
+    &:hover {
+      background-color: transparent;
+      border-bottom: 2px solid var(--color-text-secondary);
+    }
+  }
+
+  .nav-bar-theme-icon {
+    display: none;
+  }
+
+  .nav-links-theme-icon {
+    display: block;
+  }
+}
 </style>
