7588a9aa1534a6c14a4f8a8b0bc3bbf58ef185dc
jakub.urban <jakub.73@hotmail.com>
2021-02-18 10:21:39 +0100

    Add Tags

--- components/admin/posts/Tags.vue
+++ components/admin/posts/Tags.vue
@@ -1,22 +1,61 @@
 <template>
-  <div class="tags">
-    <ul v-for="tag in tags" :key="tag.id">
-      <li>{{tag.value}}</li>
-    </ul>
+  <div class="tag-wrapper">
+    <div class="tag-list">
+      <span>Skills:</span>
+      <span
+        v-for="tag in tags"
+        :key="tag.id"
+        class="badge"
+        v-bind:class="[tag.value.toLowerCase()]"
+        >{{ tag.value }}</span
+      >
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      tags: {
-        type: Object,
-        default: {}
-      },
+export default {
+  data() {
+    return {
+      badgeClass: '',
+    }
+  },
+  props: {
+    tags: {
+      type: Object,
+      default: {},
     },
-  }
+  },
+}
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.tag-wrapper {
+  padding: 2rem 0 2rem 0;
+}
 
+.tag-list {
+  display: inline;
+}
+
+.badge {
+  background-color: #fff;
+  color: black;
+  border-radius: 0.5rem;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  padding: 0.5em;
+}
+
+.html {
+  background-color: #e85d25;
+}
+
+.css {
+  background-color: #25a1e1;
+}
+
+.js {
+  background-color: #e5a329;
+}
 </style>
