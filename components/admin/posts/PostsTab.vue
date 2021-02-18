69dc8ffd836b47e39fb63653d2420a705431fce3
jakub.urban <jakub.73@hotmail.com>
2021-02-18 13:18:47 +0100

    routing from table posts to page with single post

--- components/admin/posts/PostsTab.vue
+++ components/admin/posts/PostsTab.vue
@@ -2,18 +2,19 @@
   <div class="table-wrapper">
     <table>
       <thead>
+<th>id</th>
         <th>Title</th>
         <th>SLug</th>
         <th>Ceated at</th>
         <th>Link</th>
       </thead>
       <tbody>
-        <tr v-for="post in postList" :key="post.id" >
+        <tr v-for="post in postList" :key="post.id">
+          <td>{{ post.id }}</td>
           <td>{{ post.title }}</td>
           <td>{{ post.slug }}</td>
           <td>{{ dateFormat(post.createdAt) }}</td>
-          <td><NuxtLink to="/admin/posts/first-post">Link</NuxtLink></td>
-
+          <td><NuxtLink :to="{ name: 'admin-posts-slug', params: { slug: post.id }}">Link</NuxtLink></td>
         </tr>
       </tbody>
     </table>
@@ -22,25 +23,23 @@
 
 <script>
 import moment from 'moment'
-  export default {
-    props: {
-      postList: {
-        type: Object,
-        default: {}
-      },
+export default {
+  props: {
+    postList: {
+      type: Object,
+      default: {},
     },
-    methods: {
-      dateFormat(datetime) {
-        return moment(datetime).fromNow()
-      }
+  },
+  methods: {
+    dateFormat(datetime) {
+      return moment(datetime).fromNow()
     },
-  }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
-.table-wrapper{
+.table-wrapper {
   margin: 5rem auto;
 }
-
 </style>
--- pages/admin/posts/_slug.vue
+++ pages/admin/posts/_slug.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
-
+    {{ postList }}
+    {{ urlParam }}
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      title: 'Dashboard',
+      postList: {},
+      urlParam: ''
+    }
+  },
+
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+  mounted() {
+    this.getAllPosts($nuxt.$route.params.slug)
+  },
+  methods: {
+    getAllPosts(params) {
+      axios
+        .get(`http://localhost:5000/api/posts/${params}`)
+        .then((response) => {
+          console.log(response)
+          this.postList = response.data
+        })
+        .catch(console.error())
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
