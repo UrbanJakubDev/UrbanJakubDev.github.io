47c3d89e38266b2c33c32433c312ba33a26a3029
jakub.urban <jakub.73@hotmail.com>
2021-04-07 14:08:12 +0200

    delete dist

--- dist/_nuxt/static/1616491340/admin/post/create/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FUrbanJakubDev.github.io\u002F_nuxt\u002Fstatic\u002F1616491340",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin\u002Fpost\u002Fcreate",config:{content:{dbHash:"b25b294c"}}}}(null));
--- dist/_nuxt/static/1616491340/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FAbout","\u002Fportfolio","\u002FLogin","\u002FCv","\u002Fcv","\u002Fabout"]})
